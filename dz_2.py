639b30e4d400add36a491da905215143c1214407
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-04-10 21:06:24 +0300

    zadanie 2 :wq

---
+++ dz_2.py
@@ -0,0 +1,11 @@
+s = int(input('Введите количество журавликов: '))
+
+# Serega = 1
+# Petya = 1
+# Katya = (Serega + Petya) * 2
+# print(Serega, Petya, Katya)
+
+Serega = int(s / 6)
+Petya = int(s / 6)
+Katya = (Serega + Petya) * 2
+print(Serega, Petya, Katya)
